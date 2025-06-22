21dde0d33a37b2915d36d76a7340642f1cfc64ce
RJ <rleyva@tutanota.com>
2025-06-22 11:15:13 -0400

    chore(eslint): configure eslint with astro and typescript support

---
+++ eslint.config.mjs
@@ -0,0 +1,30 @@
+import astro from 'eslint-plugin-astro';
+import tsParser from '@typescript-eslint/parser';
+
+export default [
+  ...astro.configs['flat/recommended'],
+
+  {
+    files: ['**/*.astro'],
+    languageOptions: {
+      parser: astro.parser,
+      parserOptions: {
+        parser: tsParser,
+        extraFileExtensions: ['.astro'],
+        ecmaVersion: 'latest',
+        sourceType: 'module'
+      }
+    }
+  },
+
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module'
+      }
+    }
+  }
+];
